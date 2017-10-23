64dd9c0703c08bcfd3de9d7bddb92bb429ef9511
Carlos Baeza Negroni <desarrolloweb@lacuarta.cl>
2017-10-23 12:56:56 -0400

    nueva prueba de pattern

--- data.php
+++ data.php
@@ -5,5 +5,28 @@ $data = 'some long string of hex';
 
 
 // tried echoing it directly..
-echo file_get_contents('assets/img/patronn.jpg');
+//echo file_get_contents('assets/img/patronn.jpg');
+
+echo img2hex('assets/img/patronn.jpg');
+
+
+
+
+
+ function img2hex($image) {  
+ 	$hex = '';
+ 	
+   if ($fp = fopen($image, 'r')) {  
+       $data = fread($fp, filesize($image));  
+       fclose($fp);  
+
+       for($i=0;$i<strlen($data);$i++) { 
+           $a = ord($data[$i]); 
+           $hex .= ($a <= 15) ? "0".dechex($a) : dechex($a); 
+       } 
+    } else  
+       $hex = "File error";  
+    return $hex;  
+ } 
+
 ?>
